ce1e1192482faed0fd39fec9a17174b707fd2eef
CraigAdlam <cadlam87@gmail.com>
2026-04-13 12:24:13 -0700

    Testing out 2 buttons: filtered and full

--- pages/4_Skater_Faceoff_Wins.py
+++ pages/4_Skater_Faceoff_Wins.py
@@ -176,10 +176,24 @@ st.dataframe(
     height=table_height
 )
 
-st.download_button(
-    "Download filtered data",
-    filtered_df.to_csv(index=False),
-    "filtered_skater_faceoffwins.csv",
-    "text/csv",
-    key="faceoffwins_download"
-)
+st.markdown("### Download Options")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.download_button(
+        "Filtered",
+        filtered_df.to_csv(index=False),
+        "filtered_skater_faceoffwins.csv",
+        "text/csv",
+        key="faceoffwins_download_filtered"
+    )
+
+with col2:
+    st.download_button(
+        "Full Dataset",
+        df.to_csv(index=False),
+        "full_skater_faceoffwins.csv",
+        "text/csv",
+        key="faceoffwins_download_full"
+    )
